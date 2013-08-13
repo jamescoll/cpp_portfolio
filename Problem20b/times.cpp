30fd3a1fa7cb4f4b19fcd8c9728d46828538824c
James Evin Coll <jamescoll@Jamess-MacBook-Pro.local>
2013-08-13 01:19:51 +0100

    changed faulty code

--- Problem20b/times.cpp
+++ Problem20b/times.cpp
@@ -1,124 +1,81 @@
 //============================================================================
-// Name        : Problem20b
-// Author      : James Coll
-// Version     :
-// Copyright   : Munnki
+// Name : Problem20b
+// Author : James Coll
+// Version :
+// Copyright : Munnki
 // Description : Simple program that defines a typedef time and allows for addition and subtraction (assumed to mean difference)
 //============================================================================
 
 #include <iostream>
-#include <cstdlib>
 using namespace std;
 
 typedef struct
 {
-	int iSeconds;
-	int iMinutes;
-	int iHours;
+    int iSeconds;
+    int iMinutes;
+    int iHours;
 } MyTime;
 
-MyTime mytimeAdd_Time(MyTime t1, MyTime t2);
-MyTime mytimeSubtract_Time(MyTime t1, MyTime t2);
 
 
-int main() {
-	MyTime t1, t2;
-	MyTime tsum, tsub;
-	t1.iSeconds = 34;
-	t1.iMinutes = 56;
-	t1.iHours = 12;
-	t2.iSeconds = 24;
-	t2.iMinutes = 37;
-	t2.iHours = 7;
+void convertSeconds(int iTotalSecs, MyTime& t1);
 
-	tsum = mytimeAdd_Time(t1, t2);
-	tsub = mytimeSubtract_Time(t1, t2);
+void addTimes(const MyTime& t1, const MyTime& t2, MyTime& sum);
 
-	cout<<"Time 1 "<<t1.iHours<<" Hrs "<<t1.iMinutes<<" Mns "<<t1.iSeconds<<" snds"<<endl;
-	cout<<"Time 2 "<<t2.iHours<<" Hrs "<<t2.iMinutes<<" Mns "<<t2.iSeconds<<" snds"<<endl;
-	cout<<"Sum "<<tsum.iHours<<" Hrs "<<tsum.iMinutes<<" Mns "<<tsum.iSeconds<<" snds"<<endl;
-	cout<<"Diff "<<tsub.iHours<<" Hrs "<<tsub.iMinutes<<" Mns "<<tsub.iSeconds<<" snds"<<endl;
-}
+int iTotalSeconds(const MyTime& t1);
 
-MyTime mytimeAdd_Time(MyTime t1, MyTime t2)
+
+void convertSecs(int iTotalSecs, MyTime& t1)
 {
-	MyTime tsum;
-	int iCheck = 0;
-	//first we'll add the times together
-	tsum.iSeconds = t1.iSeconds + t2.iSeconds;
-	tsum.iMinutes = t1.iMinutes + t2.iMinutes;
-	tsum.iHours = t1.iHours + t2.iHours;
-
-	//check the times starting with the seconds
-	while(tsum.iSeconds>60)
-	{
-		tsum.iSeconds -= 60;
-		iCheck++;
-	}
-
-	while(iCheck != 0)
-	{
-		tsum.iMinutes += 1;
-		iCheck--;
-	}
-
-	//check the times now the minutes
-	while(tsum.iMinutes>60)
-	{
-		tsum.iMinutes -= 60;
-		iCheck++;
-	}
-
-	while(iCheck != 0)
-	{
-		tsum.iHours += 1;
-		iCheck--;
-	}
-	//job done
-	return tsum;
+    iTotalSecs = abs(iTotalSecs);
+    int iRemainingSeconds;
+    t1.iHours = iTotalSecs/(60*60);
+    iRemainingSeconds = iTotalSecs%(60*60);
+    
+    t1.iMinutes = iRemainingSeconds/60;
+    iRemainingSeconds = iRemainingSeconds%60;
+    
+    t1.iSeconds = iRemainingSeconds;
 }
 
-//this function will always return a positive value
-//as a negative time is somewhat meaningless as such
-//it will use absolute values
-MyTime mytimeSubtract_Time(MyTime t1, MyTime t2)
+int iTotalSeconds(const MyTime& t1)
 {
-
-	MyTime tsub;
-	int iCheck = 0;
-	//first we'll add the times together
-	tsub.iSeconds = abs(t1.iSeconds - t2.iSeconds);
-	tsub.iMinutes = abs(t1.iMinutes - t2.iMinutes);
-	tsub.iHours = abs(t1.iHours - t2.iHours);
-
-	//check the times starting with the seconds
-	while(tsub.iSeconds>60)
-	{
-		tsub.iSeconds -= 60;
-		iCheck++;
-	}
-
-	while(iCheck != 0)
-	{
-		tsub.iMinutes += 1;
-		iCheck--;
-	}
-
-	//check the times now the minutes
-	while(tsub.iMinutes>60)
-	{
-		tsub.iMinutes -= 60;
-		iCheck++;
-	}
-
-	while(iCheck != 0)
-	{
-		tsub.iHours += 1;
-		iCheck--;
-	}
-	//job done
-
-	return tsub;
+    return t1.iHours*60*60+t1.iMinutes*60+t1.iSeconds;
 }
+
+void subtractTimes(const MyTime& t1, const MyTime& t2, MyTime& sum)
+{
+    int iTotal1=iTotalSeconds(t1);
+    int iTotal2=iTotalSeconds(t2);
+    convertSecs(iTotal1-iTotal2, sum);
+}
+
+
+void addTimes(const MyTime& t1, const MyTime& t2, MyTime& sum)
+{
+    int iTotal1=iTotalSeconds(t1);
+    int iTotal2=iTotalSeconds(t2);
+    convertSecs(iTotal1+iTotal2, sum);
+}
+
+int main()
+{
+    MyTime t1,t2,t3, t4;
+    
+    t1.iHours=1; t1.iMinutes=20;t1.iSeconds=30;
+    t2.iHours=2; t2.iMinutes=50;t2.iSeconds=50;
+    
+    addTimes(t1,t2, t3);
+    cout<<"Addition";
+    cout << t3.iHours << "h:" << t3.iMinutes << "m:"
+    << t3.iSeconds << "s" << endl;
+    
+    subtractTimes(t1,t2,t4);
+    cout<<"Subtraction";
+    cout << t4.iHours << "h:" << t4.iMinutes << "m:"
+    << t4.iSeconds << "s" << endl;
+}
+
+
 
 
